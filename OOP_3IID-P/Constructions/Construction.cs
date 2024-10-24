44e4d525654843e14f559aa5140d2959766af74b
Tomasz Rajzer <trajzer@wsiz.edu.pl>
2024-10-24 15:05:30 +0200

    init

---
+++ OOP_3IID-P/Constructions/Construction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_3IID_P.Constructions
+{
+    internal class Construction
+    {
+        public Construction()
+        {
+            
+        }
+
+        public Construction(string buildMaterial, float width, float height, int entrances, int humanCapacity)
+        {
+            BuildMaterial = buildMaterial;
+            Width = width;
+            Height = height;
+            Entrances = entrances;
+            HumanCapacity = humanCapacity;
+        }
+
+        public string BuildMaterial { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public int Entrances { get; set; }
+        public int HumanCapacity { get; set; }
+    }
+}
---
+++ OOP_3IID-P/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP_3IID_P.Constructions;
+
+Console.WriteLine("Hello, World!");
+var house = new Construction();
+var house2 = new Construction("Brick", 23, 45, 4, 5);
+
+
